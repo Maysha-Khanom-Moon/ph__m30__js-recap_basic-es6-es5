692fb24539633e4ec7fadc30262993ecaeea3732
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-02-23 15:15:20 +0600

    no argument then takes default value

--- default.js
+++ default.js
@@ -1,4 +1,18 @@
-function add(num1, num2) {
+// function add(num1, num2) {
+
+//     const result = num1 + num2;
+
+//     console.log(num1, num2, result);
+
+//     return result;
+// }
+
+// const sum1 = add(3, 4);
+
+// const sum2 = add();
+
+
+function add(num1, num2 = 0) {
 
     const result = num1 + num2;
 
@@ -10,4 +24,6 @@ function add(num1, num2) {
 const sum1 = add(3, 4);
 
 // default --> if value is not provided, take this as a default
-const sum2 = add();
+const sum2 = add(3, );
+
+const sum3 = add();
