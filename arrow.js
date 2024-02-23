7165d5ceb1d0c09795f1b32c04248ba011aeba49
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-02-23 21:18:58 +0600

    introduction to arrow function

---
+++ arrow.js
@@ -0,0 +1,16 @@
+// function declaration
+function add (a, b) {
+    return a + b;
+}
+
+
+// function expression
+const add2 = function (a, b) {
+    return a + b;
+}
+
+
+// arrow function
+const add3 = (a, b) => a+b;
+
+console.log(add3(3, 4));
