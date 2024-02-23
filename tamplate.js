cf18e8ddccadeda8dc1d24b27cfaafb1f29e322c
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-02-23 15:30:24 +0600

    backtick used to declare multiline string

--- tamplate.js
+++ tamplate.js
@@ -7,3 +7,15 @@ console.log(result);
 const math = `The sum of ${a} and ${b} is ${a+b}`;
 console.log(math);
 
+
+// multiline string
+const email = 'Maysha \n' +
+'Khanom \n' +
+'Moon';
+
+console.log(email);
+
+const EMAIL = `Maysha
+Khanom
+Moon`;
+console.log(EMAIL);
