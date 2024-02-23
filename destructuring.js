88b9556fef286b5c9d0daeb02a9a1357db5f4542
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-02-23 22:08:21 +0600

    destructuring in an array

--- destructuring.js
+++ destructuring.js
@@ -14,3 +14,25 @@ const {phone, name: nam} = actor;
 
 console.log(phone);
 console.log(nam);
+
+
+
+// -------- array destructuring ---------
+// if right side is an array left side of destructuring will be array as well
+
+const numbers = [45, 99];
+const [first, second] = numbers;
+
+// to skip elements
+const [x, , y] = [4, 5, 4];
+
+console.log(first);
+console.log(second);
+
+//
+function doubleThem(a, b) {
+    return [a*2, b*2];
+}
+
+const [prothom, ditiyo] = doubleThem(x, y);
+console.log(prothom, ditiyo);
